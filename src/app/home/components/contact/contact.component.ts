2d71b9dd804d046b66be357e267ccc4325321390
Nicolas Alba <nicolas.alba@fundacion-jala.org>
2022-11-19 12:04:52 -0400

    Adding ci github actions

--- src/app/home/components/contact/contact.component.ts
+++ src/app/home/components/contact/contact.component.ts
@@ -1,15 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent{
+export class ContactComponent {
 
   constructor() {}
-
-  // ngOnInit(): void {
-
-  // }
 }
